75471f5b2a8a85a7e7a95a025a12e748ba91e4b4
Tasdik Rahman <87649474+tasdikrahman-deliveryhero@users.noreply.github.com>
2021-08-23 14:09:16 +0530

    (OGSYS-1188): Adding support for 20.04 LTS ubuntu release (#10)

--- user_data/setup_init.sh
+++ user_data/setup_init.sh
@@ -5,7 +5,9 @@ echo "#### Running setup_init.sh"
 echo "#### Setting up repos and software"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install --force-yes -y python-pip python-setuptools awscli
+# as python 2 is deprecated in focal release (20.04 LTS) of ubuntu
+apt-get install --force-yes -y python3-pip python3-setuptools awscli
+# pip3 and pip point to the same binary for 20.04
 pip install --upgrade pip
 ######################################################################
 echo "#### Setting hostname"
